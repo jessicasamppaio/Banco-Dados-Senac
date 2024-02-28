357b581077b021347289f320cfb136221811ba7a
Jéssica <jessicaspf@gmail.com>
2024-02-27 21:19:14 -0300

    Atualizando sql da aula 10

--- aulas/09/DQL.sql
+++ aulas/09/DQL.sql
@@ -1,11 +1,58 @@
-SELECT Nome
-FROM Aluno
-WHERE Matricula < 10
-ORDER BY Nome;
+-- ----------------
+-- Consultas Básicas
+-- -----------------
 
-SELECT * FROM Aluno; 
+SELECT * FROM Aluno;
 
-SELECT A.Nome FROM Aluno A;
-SELECT PF.Professor_ID FROM Professor_Disciplinas PF;
+-- SELECT
+-- FROM 
+-- WHERE
+-- ORDER BY
 
-SELECT Nome as nm FROM Aluno;
+SELECT *
+FROM aluno
+WHERE matricula > 3
+ORDER BY nome;
+
+-- Operadores Matemáticos e Lógicos:
+SELECT * FROM disciplina;
+
+SELECT Nome, Horas
+FROM disciplina
+WHERE horas >= 40 AND horas <= 50;
+
+-- Aliases para Melhor Legibilidade
+SELECT nome AS nome_disciplina, horas AS carga_horaria
+FROM disciplina;
+
+-- Exiba da tabela professor, todos os professores por ordem alfabetica
+-- onde o professor tenha ID maior do que 2 e menor do que 6
+-- não exibindo especialização e mudando a coluna "Nome" para "Nome_Professor"
+
+SELECT Nome AS nome_professor, ID
+FROM professor 
+WHERE id > 2 AND id < 6
+ORDER BY nome;
+
+-- Exemplo: Contar quantas disciplinas existem
+
+SELECT COUNT(*) as QuantidadeDeDisciplinas
+FROM disciplina
+WHERE horas > 50;
+
+-- Me informe qunatas matriculas foram feitas no curso de ID 2
+
+SELECT COUNT(*) as QuantidadeMatriculas
+FROM matricula
+WHERE Curso_ID = 2;
+
+-- Exemplo: Selecionar professor com especialização que envolva Tecnologia
+SELECT * 
+FROM professor
+WHERE especializacao LIKE '%Tecnologia%';
+
+-- Exercicio: Selecione todos os cursos que terminem com "medicina" ou começa com a letra "D"
+
+SELECT *
+FROM curso
+WHERE nome LIKE '%Medicina' OR nome LIKE 'D%';
