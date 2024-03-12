8fd569ee59c693a028c9042be4f9357fed5a6fbd
Jéssica <jessicaspf@gmail.com>
2024-03-11 22:03:23 -0300

    Atualiazando com atividade da aula 19

---
+++ aulas/19/atividade.sql
@@ -0,0 +1,17 @@
+# Criar Usuário e Atribuir Permissões
+-- Crie um usuário chamado 'usuario_vendas' com a senha 'senha*123'
+-- Conceda a esse usuário todas as permissões no banco de dados 'compras'
+
+# Ajustar Permissões Específicas
+-- Cire um usuário chamado 'usuario_marketing' com a senha 'Senha_456'
+-- Conceda a esse usuário a permissão SELECT apenas na tabela 'Produto' do bd 'compras'
+
+# Gerenciar Acesso ao ItensPedido
+-- Crie um usuário chaamdo 'usuario_consulta' com a senha 'consultA#123'
+-- Conceda a esse usuário permissões de consulta em todas as tabelas do bd 'compras'
+-- Verifique as permissões conceidades ao usuário e exiba as informações
+
+# Revogar Permissões
+-- Crie um usuário chamado 'usuario_limitado' com a senha 'Limite%123'
+-- Conceda a esse usuário permissão SELECT apenas na tabela 'cliente' do bd 'compras'
+-- Revogue a permissão delete para esse usuário na tabela 'cliente'
